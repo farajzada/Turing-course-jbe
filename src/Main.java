678bac36b9914eb346748b04c8c54aa13d2a9841
muradfarajzada <126752911+muradfarajzada@users.noreply.github.com>
2024-10-26 20:28:08 +0400

    isPositive code added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!111");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        boolean isPositive = checkPositiveInteger(a);
+        System.out.println(isPositive);
+    }
+
+    static boolean checkPositiveInteger(int a) {
+        return a > 0;
     }
 }
